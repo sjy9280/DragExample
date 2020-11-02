c408cd3bbd3e459d39a594aab5d773a0b01754b6
Joanna <sjy9280@163.com>
2020-11-02 14:21:12 +0800

    add example

--- app/src/main/java/com/joanna/dragexample/ChatActivity.kt
+++ app/src/main/java/com/joanna/dragexample/ChatActivity.kt
@@ -3,9 +3,9 @@ package com.joanna.dragexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_drag_example)
     }
 }
---
+++ app/src/main/java/com/joanna/dragexample/widget/MyViewDragHelper.java
@@ -0,0 +1,80 @@
+package com.joanna.dragexample.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.customview.widget.ViewDragHelper;
+
+public class MyViewDragHelper extends FrameLayout {
+
+    private ViewDragHelper mViewDragHelper;
+    private int mDragOriLeft;
+    private int mDragOriTop;
+
+    public MyViewDragHelper(@NonNull Context context) {
+        this(context,null);
+    }
+
+    public MyViewDragHelper(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public MyViewDragHelper(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mViewDragHelper = ViewDragHelper.create(this, new ViewDragHelper.Callback() {
+            @Override
+            public boolean tryCaptureView(@NonNull View child, int pointerId) {
+                return true;
+            }
+
+            @Override
+            public void onViewCaptured(@NonNull View capturedChild, int activePointerId) {
+                super.onViewCaptured(capturedChild, activePointerId);
+                mDragOriLeft = capturedChild.getLeft();
+                mDragOriTop = capturedChild.getTop();
+            }
+
+            @Override
+            public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
+                super.onViewReleased(releasedChild, xvel, yvel);
+                mViewDragHelper.settleCapturedViewAt((int)mDragOriLeft,(int)mDragOriTop);
+                invalidate();
+            }
+
+            @Override
+            public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
+                return left;
+            }
+
+            @Override
+            public int clampViewPositionVertical(@NonNull View child, int top, int dy) {
+                return top;
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return mViewDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mViewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mViewDragHelper != null && mViewDragHelper.continueSettling(true)){
+            invalidate();
+        }
+    }
+}
